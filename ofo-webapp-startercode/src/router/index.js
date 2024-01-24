026caf50e1fb77f2b5da2d432dc148fd9b54a9aa
StefanStrezoski <stefan.strezo98@gmail.com>
2024-01-24 02:16:44 +0100

    Added default route to '/' path

--- ofo-webapp-startercode/src/router/index.js
+++ ofo-webapp-startercode/src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../views/HomePage.vue'
 import UploadPage from "@/views/UploadPage.vue";
 import PanelPage from "@/views/PanelPage.vue";
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomePage
+    redirect: '/panel',
   },
   {
     path: '/upload',
--- ofo-webapp-startercode/src/views/HomePage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-card>
-    <v-card-title>Start Here</v-card-title>
-  </v-card>
-</template>
