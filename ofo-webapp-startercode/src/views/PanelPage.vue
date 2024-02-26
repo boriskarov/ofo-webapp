d68ffb36597b61da18757e7a732f3a3f41e81232
StefanStrezoski <stefan.strezo98@gmail.com>
2024-02-26 23:07:46 +0100

    Modifying PanelPage to satisfy API calls

--- ofo-webapp-startercode/src/views/PanelPage.vue
+++ ofo-webapp-startercode/src/views/PanelPage.vue
@@ -14,7 +14,13 @@ export default{
     },
     editTask(task){
       this.$router.push(`/tasks/${task.id}/edit`);
+    },
+    loadTasks(){
+      this.$store.dispatch('tasks/loadTasks');
     }
+  },
+  mounted() {
+    this.loadTasks()
   }
 }
 </script>
@@ -22,33 +28,33 @@ export default{
 <template>
 <div >
   <v-card-title class="ma-5 flex text-center">Pending tasks</v-card-title>
-  <v-container>
+  <v-container v-if="tasks.length>0">
     <v-card class="ma-1 pa-4 bg-blue-grey-lighten-5" v-for="task in tasks" :key="task.id">
-      <v-layout :class="`pa-4 ${task.status}`">
+      <v-layout :class="`pa-4 ${task.ticketStatus}`">
         <v-row>
           <v-col cols="2">
             <div class="text-caption">
-              {{ task.name }}
+              {{ task.subject }}
             </div>
             <div>
-              {{ task.description }}
+              {{ task.body }}
             </div>
           </v-col>
           <v-col cols="2">
             <div class="text-caption">Due date</div>
             <div>
-              {{ task.due }}
+              {{ task.createdAt }}
             </div>
           </v-col>
           <v-col cols="2">
-            <v-chip :class="`chip-${task.status}`">
-              {{ task.status }}
+            <v-chip :class="`chip-${task.ticketStatus}`">
+              {{ task.ticketStatus }}
             </v-chip>
           </v-col>
           <v-col cols="2">
             <v-btn @click="editTask(task)">Edit</v-btn>
           </v-col>
-          <v-col cols="2" v-if="task.status === 'inprogress'">
+          <v-col cols="2" v-if="task.ticketStatus === 'OPEN'">
             <v-btn class="bg-green-accent-4 text-white" @click="markCompleted(task)">Done</v-btn>
           </v-col>
           <v-spacer></v-spacer>
@@ -59,23 +65,33 @@ export default{
       </v-layout>
     </v-card>
   </v-container>
+  <v-container v-else>
+    <v-card class="card">
+      <v-card-title>Tasks not found!</v-card-title>
+      <v-card-text>Please add some tickets</v-card-text>
+    </v-card>
+  </v-container>
 </div>
 </template>
 
 <style scoped>
-.chip-completed{
+.chip-RESOLVED{
   background-color: #0ef622;
 }
-.chip-inprogress{
+.chip-OPEN{
   background-color: coral;
 }
 
-.completed{
+.RESOLVED{
   border-left: 6px solid #0ef622;
 }
 
-.inprogress{
+.OPEN{
   border-left: 5px solid coral;
 }
-
+.card{
+  margin: 1rem auto;
+  max-width: 55rem;
+  text-align: center;
+}
 </style>
