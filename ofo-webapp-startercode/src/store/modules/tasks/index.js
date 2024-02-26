7621047c457781e65048adea610b75a6d4307c55
StefanStrezoski <stefan.strezo98@gmail.com>
2024-02-26 23:26:09 +0100

    Fixed tasks gloabal state variable

--- ofo-webapp-startercode/src/store/modules/tasks/index.js
+++ ofo-webapp-startercode/src/store/modules/tasks/index.js
@@ -6,37 +6,7 @@ export default {
     namespaced: true,
     state(){
         return {
-            tasks: [
-                {
-                    id: 1,
-                    name: 'Task1',
-                    description: 'Create global store',
-                    due: '02.02.2024',
-                    status: 'completed',
-                },
-                {
-                    id: 2,
-                    name: 'Task2',
-                    description: 'Add task overview',
-                    due: '02.02.2024',
-                    status: 'inprogress',
-                },
-                {
-                    id: 3,
-                    name: 'Task3',
-                    description: 'Create file upload interface',
-                    due: '02.02.2024',
-                    status: 'inprogress',
-                },
-                {
-                    id: 4,
-                    name: 'Task4',
-                    description: 'Create BPMN flow',
-                    due: '02.02.2024',
-                    status: 'inprogress',
-                },
-
-            ]
+            tasks: []
         }
     },
     mutations,
